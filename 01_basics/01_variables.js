b517d04d1f3dac6d07a1d4c02f35f9dc32dab3bb
Divya Rajput <88126462+Divya1S@users.noreply.github.com>
2026-05-12 07:27:44 +0000

    New file created

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId = 111234
+let accountEmail = "divya@google.com"
+var accountPassword = "1234"
+accountCity = "Los Angeles" //This should be used but a variable can be declared in this way as well
+let accountState;
+
+//accountId = 21 //not allowed 
+
+// {
+//     Scope
+// }
+
+// Initially same variable is used then the first variable would change 
+
+accountEmail = "pooja@gmail.com"
+
+accountPassword = "98768456"
+
+accountCity = "NewYork"
+
+console.log(accountId);
+
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var 
+because of issue in block scope and funtional scope
+*/
+
