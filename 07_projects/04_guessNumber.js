5d56cc47aa6b11c77e0e570e4bb1b5e7f287cea3
Divya Rajput <88126462+Divya1S@users.noreply.github.com>
2026-06-09 02:27:02 +0000

    guessNumber

---
+++ 07_projects/04_guessNumber.js
@@ -0,0 +1,29 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHigh = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+function validateGuess(guess){
+
+}
+
+function checkGuess(guest){
+
+}
+
+function displayGuess(guest){
+  
+}
+
+function displayMessage(message){
+
+}
