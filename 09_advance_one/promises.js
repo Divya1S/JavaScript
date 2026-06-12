bd882baed4d9b261b6f307569c6ad75a6dc2caf1
Divya Rajput <88126462+Divya1S@users.noreply.github.com>
2026-06-12 04:25:25 +0000

    promises part2

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -42,11 +42,59 @@ const promiseFour = new Promise(function(resolve, reject){
     }, 1000)
 })
 
-promiseFour.then((user) => {
+promiseFour
+.then((user) => {
     console.log(user);
     return user.username
-}).then((username) => {
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
 })
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+const promiseFive = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = true;
+        if (!error) {
+            resolve({username: "javscript", password: "321"})
+        }else {
+            reject('ERROR: Javascript went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         // console.log(response);
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error)
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
