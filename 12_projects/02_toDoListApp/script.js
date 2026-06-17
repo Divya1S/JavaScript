46a9855ee8f9f1e42e3a85fd8a6889d3b19fa7c3
Divya Rajput <88126462+Divya1S@users.noreply.github.com>
2026-06-17 06:27:41 +0000

    toDoList App complete

--- 12_projects/02_toDoListApp/script.js
+++ 12_projects/02_toDoListApp/script.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+//Add task function
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something");
@@ -14,4 +15,39 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
+    inputBox.focus(); //put the cursor back in the box
+    saveData(); //Ensure empty string doesn't clear storage
 }
+
+
+//Check or delete task
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+// Keyboard support (Press Enter)
+inputBox.addEventListener("keypress", function(event){
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
+
+//Peristent: Save and Load
+//Save data to localStorage whenever the list changes
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+//Display data when the page loads
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+//Call showTask() at the end of script
+showTask();
